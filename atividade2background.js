7f5803c1b395a4b5fc08987f7e6d6e76d7537055
Guijf007 <124809960+Guijf007@users.noreply.github.com>
2023-08-04 16:24:28 -0300

    Add files via upload

---
+++ atividade2background.js
@@ -0,0 +1,9 @@
+function trocar1(){
+    document.getElementById('d1').setAttribute("style", "background-image:url('memetio.png')");
+}
+function trocar2(){
+    document.getElementById('d1').setAttribute("style", "background-image:url('cassio.png')");
+}
+function trocar3(){
+    document.getElementById('d1').setAttribute("style", "background-image:url('cassio2.jpg')");
+}
